2b6fb5d9999116edf680e4f7826ff0299f7da0a6
Julieta Gordillo <julietagordillo13@hotmail.com>
2023-11-24 12:00:04 -0300

    finish work logic

--- src/components/Buttons/ButtonUp.jsx
+++ src/components/Buttons/ButtonUp.jsx
@@ -1,16 +1,15 @@
-import * as React from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import ButtonUpIcon from "../../assets/img/Buttons/ButtonUpIcon";
+import UpIcon from "../../assets/img/Buttons/upIcon";
 import "../../styles/ButtonUp.css";
 
-const ButtonUp = () => (
-  <Link to="/">
-    <button
-      className="ButtonUp-container"
-      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-    >
-      <ButtonUpIcon />
-    </button>
-  </Link>
-);
+const ButtonUp = () => {
+  return (
+    <Link to="/">
+      <div className="button-up" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
+        <UpIcon /> 
+      </div>
+    </Link>
+  );
+};
 export default ButtonUp;
--- src/components/Works/Moreno.jsx
+++ src/components/Works/Moreno.jsx
@@ -21,26 +21,31 @@ const Moreno = () => {
 
   const handlePrevClick = () => {
     const prevIndex = currentIndex - 1;
-    const prevComponent =
-      components.find((item) => item.index === prevIndex) || {};
-    setCurrentComponent(prevComponent);
-    setCurrentIndex(prevIndex);
+    if (currentIndex > 1) {
+      setCurrentIndex(currentIndex - 1);
+      const prevComponent =
+        components.find((item) => item.index === prevIndex) || {};
+      setCurrentComponent(prevComponent);
+    }
   };
 
   const handleNextClick = () => {
     const nextIndex = currentIndex + 1;
-    const nextComponent =
-      components.find((item) => item.index === nextIndex) || {};
-    setCurrentComponent(nextComponent);
-    setCurrentIndex(nextIndex);
+    if (nextIndex <= components.length) {
+      setCurrentIndex(nextIndex);
+      const nextComponent =
+        components.find((item) => item.index === nextIndex) || {};
+      setCurrentComponent(nextComponent);
+    }
   };
   /* const navigateTo = (index) => {
     navigate(`/work/${index}`);
   }; */
-
+  const isFirstComponent = currentIndex === 0;
+  const isLastComponent = currentIndex === components.length - 1;
   return (
     <div>
-      <Prev onClick={handlePrevClick} />
+      <Prev onClick={handlePrevClick} disabled={isFirstComponent}/>
       <ItemTitle
         number={currentComponent.index}
         text={currentComponent.title}
@@ -49,7 +54,7 @@ const Moreno = () => {
         textClass="Work-TextIndividual"
         active={true}
       />
-     <Next onClick={handleNextClick} />
+     <Next onClick={handleNextClick} disabled={isLastComponent} />
       <p>{currentComponent.description}</p>
       <p>{currentComponent.date}</p>
     </div>
