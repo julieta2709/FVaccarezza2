83c19387d5cc945e1b4e813805b545464e7503c4
Julieta Gordillo <julietagordillo13@hotmail.com>
2024-01-08 12:15:55 -0300

    frog styles

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -60,46 +60,52 @@ const Photos = () => {
       <img src={pic6} alt="paisaje6" className="Photo-item6" />
       <img src={pic7} alt="paisaje7" className="Photo-item7" />
       <img src={pic8} alt="paisaje8" className="Photo-item8" />
-      <img src={c1} alt="foto1" className="Photo-c1" />
-      <img src={c2} alt="foto2" className="Photo-c2" />
-      <img src={c3} alt="foto3" className="Photo-c3" />
-      <img src={c5} alt="foto5" className="Photo-c5" />
-      <img src={c6} alt="foto6" className="Photo-c6" />
-      <img src={c7} alt="foto7" className="Photo-c7" />
-      <img src={c8} alt="foto8" className="Photo-c8" />
-      <img src={c9} alt="foto9" className="Photo-c9" />
-      <img src={c10} alt="foto10" className="Photo-c10" />
-      <img src={c11} alt="foto11" className="Photo-c11" />
-      <img src={c12} alt="foto12" className="Photo-c12" />
-      <img src={c13} alt="foto13" className="Photo-c13" />
-      <img src={c14} alt="foto14" className="Photo-c14" />
-      <img src={d1} alt="foto1" className="Photo-d1" />
-      <img src={d2} alt="foto2" className="Photo-d2" />
-      <img src={d3} alt="foto3" className="Photo-d3" />
-      <img src={d4} alt="foto4" className="Photo-d4" />
-      <img src={d5} alt="foto5" className="Photo-d5" />
-      <img src={d6} alt="foto6" className="Photo-d6" />
-      <img src={d7} alt="foto7" className="Photo-d7" />
-      <img src={d8} alt="foto8" className="Photo-d8" />
-      <img src={d9} alt="foto9" className="Photo-d9" />
-      <img src={d10} alt="foto10" className="Photo-d10" />
-      <img src={d11} alt="foto11" className="Photo-d11" />
-      <img src={d12} alt="foto12" className="Photo-d12" />
-      <img src={d13} alt="foto13" className="Photo-d13" />
-      <img src={d14} alt="foto14" className="Photo-d14" />
-      <img src={e1} alt="foto1" className="Photo-e1" />
-      <img src={e2} alt="foto2" className="Photo-e2" />
-      <img src={e3} alt="foto3" className="Photo-e3" />
-      <img src={e4} alt="foto4" className="Photo-e4" />
-      <img src={e5} alt="foto5" className="Photo-e5" />
-      <img src={e6} alt="foto6" className="Photo-e6" />
-      <img src={e7} alt="foto7" className="Photo-e7" />
-      <img src={e8} alt="foto8" className="Photo-e8" />
-      <img src={e9} alt="foto9" className="Photo-e9" />
-      <img src={e10} alt="foto10" className="Photo-e10" />
-      <img src={e11} alt="foto11" className="Photo-e11" />
-      <img src={e12} alt="foto12" className="Photo-e12" />
-      <img src={e13} alt="foto13" className="Photo-e13" />
+      <div className="Photos-c">
+        <img src={c1} alt="foto1" className="Photo-c1" />
+        <img src={c2} alt="foto2" className="Photo-c2" />
+        <img src={c3} alt="foto3" className="Photo-c3" />
+        <img src={c5} alt="foto5" className="Photo-c5" />
+        <img src={c6} alt="foto6" className="Photo-c6" />
+        <img src={c7} alt="foto7" className="Photo-c7" />
+        <img src={c8} alt="foto8" className="Photo-c8" />
+        <img src={c9} alt="foto9" className="Photo-c9" />
+        <img src={c10} alt="foto10" className="Photo-c10" />
+        <img src={c11} alt="foto11" className="Photo-c11" />
+        <img src={c12} alt="foto12" className="Photo-c12" />
+        <img src={c13} alt="foto13" className="Photo-c13" />
+        <img src={c14} alt="foto14" className="Photo-c14" />
+      </div>
+      <div className="Photos-d">
+        <img src={d1} alt="foto1" className="Photo-d1" />
+        <img src={d2} alt="foto2" className="Photo-d2" />
+        <img src={d3} alt="foto3" className="Photo-d3" />
+        <img src={d4} alt="foto4" className="Photo-d4" />
+        <img src={d5} alt="foto5" className="Photo-d5" />
+        <img src={d6} alt="foto6" className="Photo-d6" />
+        <img src={d7} alt="foto7" className="Photo-d7" />
+        <img src={d8} alt="foto8" className="Photo-d8" />
+        <img src={d9} alt="foto9" className="Photo-d9" />
+        <img src={d10} alt="foto10" className="Photo-d10" />
+        <img src={d11} alt="foto11" className="Photo-d11" />
+        <img src={d12} alt="foto12" className="Photo-d12" />
+        <img src={d13} alt="foto13" className="Photo-d13" />
+        <img src={d14} alt="foto14" className="Photo-d14" />
+      </div>
+      <div className="Photos-e">
+        <img src={e1} alt="foto1" className="Photo-e1" />
+        <img src={e2} alt="foto2" className="Photo-e2" />
+        <img src={e3} alt="foto3" className="Photo-e3" />
+        <img src={e4} alt="foto4" className="Photo-e4" />
+        <img src={e5} alt="foto5" className="Photo-e5" />
+        <img src={e6} alt="foto6" className="Photo-e6" />
+        <img src={e7} alt="foto7" className="Photo-e7" />
+        <img src={e8} alt="foto8" className="Photo-e8" />
+        <img src={e9} alt="foto9" className="Photo-e9" />
+        <img src={e10} alt="foto10" className="Photo-e10" />
+        <img src={e11} alt="foto11" className="Photo-e11" />
+        <img src={e12} alt="foto12" className="Photo-e12" />
+        <img src={e13} alt="foto13" className="Photo-e13" />
+      </div>
     </div>
   );
 };
--- src/components/Works/Frog.jsx
+++ src/components/Works/Frog.jsx
@@ -1,4 +1,15 @@
 import React, { useContext } from "react";
+import Frog1 from "../../assets/img/Frog/Frog1.png";
+import Frog2 from "../../assets/img/Frog/Frog2.png";
+import Frog3 from "../../assets/img/Frog/Frog3.png";
+import Frog4 from "../../assets/img/Frog/Frog4.png";
+import Frog5 from "../../assets/img/Frog/Frog5.png";
+import Frog6 from "../../assets/img/Frog/Frog6.png";
+import Frogcel1 from "../../assets/img/Frog/Frogcel1.png";
+import Frogcel2 from "../../assets/img/Frog/Frogcel2.png";
+import Frogcel3 from "../../assets/img/Frog/Frogcel3.png";
+import Frogcel4 from "../../assets/img/Frog/Frogcel4.png";
+import "../../styles/Frog.css";
 import "../../styles/Work.css";
 import BeLink from "../Buttons/BeLink";
 import { WorkDataContext } from "./WorkContext";
@@ -22,7 +33,21 @@ const Frog = () => {
         </div>
         <p className="WorkTitleDate">{date}</p>
       </div>
-      <BeLink link={url} />
+      <div className="Frogphoto-container">
+        <img src={Frog1} alt="FrogBazar1" className="Frog1" />
+        <img src={Frog2} alt="FrogBazar2" className="Frog2" />
+        <img src={Frog3} alt="FrogBazar3" className="Frog3" />
+        <img src={Frog4} alt="FrogBazar4" className="Frog4" />
+        <img src={Frog5} alt="FrogBazar5" className="Frog5" />
+        <img src={Frog6} alt="FrogBazar6" className="Frog6" />
+        <img src={Frogcel1} alt="FrogBazarcel1" className="Frogcel1" />
+        <img src={Frogcel2} alt="FrogBazarcel2" className="Frogcel2" />
+        <img src={Frogcel3} alt="FrogBazarcel3" className="Frogcel3" />
+        <img src={Frogcel4} alt="FrogBazarcel4" className="Frogcel4" />
+      </div>
+      <div className="Belink">
+        <BeLink link={url} />
+      </div>
     </div>
   );
 };
