1cf363d8448a8c41929bc608c1f1400b316ffe35
Julieta Gordillo <julietagordillo13@hotmail.com>
2024-01-12 11:45:04 -0300

    misc styles

--- src/components/Works/Logofolio.jsx
+++ src/components/Works/Logofolio.jsx
@@ -1,4 +1,22 @@
 import React, { useContext } from "react";
+import Chamberlain from "../../assets/img//Logofolio/Chamberlain.png";
+import Frame1 from "../../assets/img//Logofolio/Frame1.png";
+import Frame10 from "../../assets/img//Logofolio/Frame10.png";
+import Frame11 from "../../assets/img//Logofolio/Frame11.png";
+import Frame12 from "../../assets/img//Logofolio/Frame12.png";
+import Frame2 from "../../assets/img//Logofolio/Frame2.png";
+import Frame3 from "../../assets/img//Logofolio/Frame3.png";
+import Frame4 from "../../assets/img//Logofolio/Frame4.png";
+import Frame5 from "../../assets/img//Logofolio/Frame5.png";
+import Frame6 from "../../assets/img//Logofolio/Frame6.png";
+import Frame7 from "../../assets/img//Logofolio/Frame7.png";
+import Frame8 from "../../assets/img//Logofolio/Frame8.png";
+import Frame9 from "../../assets/img//Logofolio/Frame9.png";
+import Hermandad from "../../assets/img//Logofolio/Hermandad.png";
+import Possco from "../../assets/img//Logofolio/Possco.png";
+import Rimini from "../../assets/img//Logofolio/Rimini.png";
+import TheNewWave from "../../assets/img//Logofolio/TheNewWave.png";
+import "../../styles/Logofolio.css";
 import "../../styles/Work.css";
 import { WorkDataContext } from "./WorkContext";
 
@@ -24,6 +42,61 @@ const Logofolio = () => {
         </div>
       </div>
       <div className="WorkIndividualphoto-container"></div>
+      <ul className="Logo-MainContainer">
+        <li className="Logo-container">
+          <div className="LogoTitle-container">
+            <p className="Logo-number">1.</p>
+            <p className="Logo-text">THE NEW WAVE</p>
+          </div>
+          <img src={TheNewWave} alt="TheNewWave" className="Logo-img" />
+        </li>
+        <li className="Logo-container">
+          <div className="LogoTitle-container">
+            <p className="Logo-number">2.</p>
+            <p className="Logo-text">CHAMBERLAIN</p>
+          </div>
+          <img src={Chamberlain} alt="Chamberlain" className="Logo-img" />
+        </li>
+        <li className="Logo-container">
+          <div className="LogoTitle-container">
+            <p className="Logo-number">3.</p>
+            <p className="Logo-text">POSSCO</p>
+          </div>
+          <img src={Possco} alt="Possco" className="Logo-img" />
+        </li>
+        <li className="Logo-container">
+          <div className="LogoTitle-container">
+            <p className="Logo-number">4.</p>
+            <p className="Logo-text">LA HERMANDAD DEL ANZUELO</p>
+          </div>
+          <img src={Hermandad} alt="Hermandad" className="Logo-img" />
+        </li>
+        <li className="Logo-container">
+          <div className="LogoTitle-container">
+            <p className="Logo-number">5.</p>
+            <p className="Logo-text">RIMINI</p>
+          </div>
+          <img src={Rimini} alt="Rimini" className="Logo-img" />
+        </li>
+      </ul>
+      <div className="Frame-container">
+        <div className="Frame-row">
+          <img src={Frame1} alt="Frame1" className="Frame" />
+          <img src={Frame2} alt="Frame2" className="Frame" />
+          <img src={Frame3} alt="Frame3" className="Frame" />
+          <img src={Frame4} alt="Frame4" className="Frame" />
+          <img src={Frame5} alt="Frame5" className="Frame" />
+          <img src={Frame6} alt="Frame6" className="Frame" />
+        </div>
+        <div className="Frame-row">
+          <img src={Frame7} alt="Frame7" className="Frame" />
+          <img src={Frame8} alt="Frame8" className="Frame" />
+          <img src={Frame9} alt="Frame9" className="Frame" />
+          <img src={Frame10} alt="Frame10" className="Frame" />
+          <img src={Frame11} alt="Frame11" className="Frame" />
+          <img src={Frame12} alt="Frame12" className="Frame" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Works/Misc.jsx
+++ src/components/Works/Misc.jsx
@@ -1,4 +1,22 @@
 import React, { useContext } from "react";
+import Misc1 from "../../assets/img/Misc/Misc1.png";
+import Misc10 from "../../assets/img/Misc/Misc10.png";
+import Misc11 from "../../assets/img/Misc/Misc11.png";
+import Misc12 from "../../assets/img/Misc/Misc12.png";
+import Misc13 from "../../assets/img/Misc/Misc13.png";
+import Misc14 from "../../assets/img/Misc/Misc14.png";
+import Misc15 from "../../assets/img/Misc/Misc15.png";
+import Misc16 from "../../assets/img/Misc/Misc16.png";
+import Misc17 from "../../assets/img/Misc/Misc17.png";
+import Misc2 from "../../assets/img/Misc/Misc2.png";
+import Misc3 from "../../assets/img/Misc/Misc3.png";
+import Misc4 from "../../assets/img/Misc/Misc4.png";
+import Misc5 from "../../assets/img/Misc/Misc5.png";
+import Misc6 from "../../assets/img/Misc/Misc6.png";
+import Misc7 from "../../assets/img/Misc/Misc7.png";
+import Misc8 from "../../assets/img/Misc/Misc8.png";
+import Misc9 from "../../assets/img/Misc/Misc9.png";
+import "../../styles/Misc.css";
 import "../../styles/Work.css";
 import { WorkDataContext } from "./WorkContext";
 
@@ -23,7 +41,41 @@ const Misc = () => {
           <p className="WorkTitleDate">{date}</p>
         </div>
       </div>
-      <div className="WorkIndividualphoto-container"></div>
+      <div className="WorkIndividualphoto-container">
+        <div className="Misc-MainContainer">
+          <div className="Misc-section1">
+            <p className="Misc-sectionTitle">BEZIER</p>
+            <div className="section1-container">
+              <img src={Misc1} alt="Misc1" className="Misc1" />
+              <img src={Misc2} alt="Misc2" className="Misc2" />
+              <img src={Misc3} alt="Misc3" className="Misc3" />
+              <img src={Misc4} alt="Misc4" className="Misc4" />
+              <img src={Misc5} alt="Misc5" className="Misc5" />
+            </div>
+          </div>
+          <div className="Misc-section2">
+            <p className="Misc-sectionTitle">GEO</p>
+            <img src={Misc6} alt="Misc6" className="Misc" />
+            <img src={Misc7} alt="Misc7" className="Misc" />
+            <img src={Misc8} alt="Misc8" className="Misc" />
+          </div>
+          <div className="Misc-section2">
+            <p className="Misc-sectionTitle">BALANCE</p>
+            <img src={Misc9} alt="Misc9" className="Misc" />
+            <img src={Misc10} alt="Misc10" className="Misc" />
+            <img src={Misc17} alt="Misc17" className="Misc" />
+          </div>
+          <div className="Misc-section3">
+            <p className="Misc-sectionTitle">NET</p>
+            <img src={Misc11} alt="Misc11" className="Misc" />
+            <img src={Misc12} alt="Misc12" className="Misc" />
+            <img src={Misc13} alt="Misc13" className="Misc" />
+            <img src={Misc14} alt="Misc14" className="Misc" />
+            <img src={Misc15} alt="Misc15" className="Misc" />
+            <img src={Misc16} alt="Misc16" className="Misc" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
