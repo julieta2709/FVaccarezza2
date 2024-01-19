4b660423481332beee6a7147d7365d0fccde9bdf
Julieta Gordillo <julietagordillo13@hotmail.com>
2024-01-19 14:24:50 -0300

    estudio cel animation

--- src/components/Works/EstudioCelAnimation.jsx
+++ src/components/Works/EstudioCelAnimation.jsx
@@ -16,59 +16,34 @@ const EstudioCelImages = [
   Estudiocel1V6,
 ];
 
-const transitions = [
-    { duration: 1000, delay: 2000, translateY: 12.25 },
-    { duration: 500, delay: 200 },
-    { duration: 500, delay: 1 },
-    { duration: 1000, delay: 1 },
-    { duration: 1000, delay: 1, translateY: 18.99 },
-    { duration: 1000, delay: 800, changeFromY: 18.99, changeToY: 12.25 },
-  ];
-
 const EstudioCelAnimation = () => {
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const nextIndex = (index + 1) % EstudioCelImages.length;
-    const { duration, delay } = transitions[index];
-
     const timer = setTimeout(() => {
       setIndex(nextIndex);
-    }, duration + delay);
-
+    }, 3000);
     return () => clearTimeout(timer);
-  }, [index]);
+  }, [index, EstudioCelImages]);
 
-  const transitionTimingFunction = 'cubic-bezier(0.55, 0.59, 0, 1.01)';
-
-  const getTransitionStyle = () => {
-    const { duration, delay, translateY, changeFromY, changeToY } = transitions[index];
-    let transformValue = translateY ? `translateY(${translateY}%)` : '';
-
-    if (changeFromY && changeToY) {
-      transformValue = `translateY(${changeFromY}%)`;
-      setTimeout(() => {
-        transformValue = `translateY(${changeToY}%)`;
-      }, duration);
-    }
-
-    return {
-      zIndex: index === setIndex ? 4 : 3,
-      opacity: index === setIndex ? 1 : 0,
-      transition: `zIndex 2500ms ${transitionTimingFunction} 0ms, opacity 2500ms ${transitionTimingFunction} 0ms, transform ${duration}ms ${transitionTimingFunction} ${delay}ms`,
-      transform: transformValue,
-    };
-  };
+  const transitionTimingFunction = "cubic-bezier(0.55, 0.59, 0, 1.01)";
 
   return (
     <div className="animated-imageContainer">
-      {EstudioCelImages.map((image, index) => (
+      {EstudioCelImages.map((image, currentIndex) => (
         <img
-          key={index}
+          key={currentIndex}
           src={image}
-          alt={`Estudio ${index + 1}`}
-          className={`animated-Estudioimage${index + 1}`}
-          style={getTransitionStyle()}
+          alt={`Estudio ${currentIndex + 1}`}
+          className={`animated-Estudioimage${currentIndex + 1}`}
+          style={{
+            zIndex: currentIndex === index ? 4 : 3,
+            opacity: currentIndex === index ? 1 : 0,
+            transition: `
+            z-index,
+            opacity 2500ms ${transitionTimingFunction} 0ms `,
+          }}
         />
       ))}
     </div>
