ad74f50c19b45843ef8690670e7c13a296da3ea4
Julieta Gordillo <julietagordillo13@hotmail.com>
2024-01-24 15:25:31 -0300

    circle animation

--- src/components/Animations/Moreno/CircleFusion.jsx
+++ src/components/Animations/Moreno/CircleFusion.jsx
@@ -3,54 +3,49 @@ import React, { useEffect } from "react";
 import "../../../styles/Moreno.css";
 
 const CircleFusion = () => {
-  const centerCircleVariants = {
-    hidden: { scale: 0, opacity: 0 },
-    visible: { scale: 1, opacity: 1 },
-  };
-
-  const enlargedCircleVariants = {
-    hidden: { scale: 1, opacity: 1 },
-    visible: { scale: 2, fill: "#ffffff" },
-  };
-
-  const cornerCirclesVariants = {
-    hidden: { x: 2, y: 2, scale: 2, opacity: 1 },
-    visible: { x: 5, y: 5, scale: 0, opacity: 0 },
-  };
-
   const controls = useAnimation();
 
-  const animateCentralCircle = async () => {
-    // Círculo central se agranda
-    await controls.start("visible");
+  const circleTopLeftVariants = {
+    hidden: { x: 0, y: 0, scale: 1, opacity: 1 },
+    visible: { x: 5, y: 5, scale: 0, opacity: 0 },
+    exit: { x: 0, y: 0, scale: 1, opacity: 1 },
+    repeat: "infinity",
+  };
+  const circleTopRightVariants = {
+    hidden: { x: 20, y: 0, scale: 1, opacity: 1 },
+    visible: { x: 5, y: 5, scale: 0, opacity: 0 },
+    exit: { x: 20, y: 0, scale: 1, opacity: 1 },
+    repeat: "infinity",
+  };
 
-    // Espera un momento antes de iniciar la siguiente animación
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+  const circleBottomLeftVariants = {
+    hidden: { x: 0, y: 20, scale: 1, opacity: 1 },
+    visible: { x: 5, y: 5, scale: 0, opacity: 0 },
+    exit: { x: 0, y: 20, scale: 1, opacity: 1 },
+    repeat: "infinity",
+  };
 
-    // Círculo grande cambia de color a gris, se achica y desaparece
-    await controls.start({ fill: "#808080", scale: 1, opacity: 0 });
-
-    // Espera un momento antes de iniciar la siguiente animación
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    // Reinicia la animación del círculo central volviendo al estado inicial
-    controls.start(centerCircleVariants.hidden);
+  const circleBottomRightVariants = {
+    hidden: { x: 20, y: 20, scale: 1, opacity: 1 },
+    visible: { x: 5, y: 5, scale: 0, opacity: 0 },
+    exit: { x: 20, y: 20, scale: 1, opacity: 1 },
+    repeat: "infinity",
   };
 
   useEffect(() => {
     const animateCornerCircles = async () => {
       // Mueve los círculos de las esquinas hacia el centro
-      await controls.start(cornerCirclesVariants.visible);
+      await controls.start("visible", { duration: 3 });
 
       // Reinicia la animación de los círculos de las esquinas volviendo al estado inicial
-      controls.start(cornerCirclesVariants.hidden);
+      controls.start("hidden", { duration: 3 });
     };
 
     animateCornerCircles();
-  }, [controls, cornerCirclesVariants]);
+  }, [controls]);
 
   return (
-    <svg width="10" height="10" className="circle-AnimaContainer">
+    <svg width="20" height="20" className="circle-AnimaContainer">
       {/* Círculos pequeños en las esquinas */}
       <motion.circle
         cx="2"
@@ -58,8 +53,10 @@ const CircleFusion = () => {
         r="2"
         fill="#ffffff"
         animate={controls}
-        variants={cornerCirclesVariants}
+        variants={circleTopLeftVariants}
         initial="hidden"
+        exit="exit"
+        repeat="infinity"
       />
       <motion.circle
         cx="8"
@@ -67,8 +64,10 @@ const CircleFusion = () => {
         r="2"
         fill="#ffffff"
         animate={controls}
-        variants={cornerCirclesVariants}
+        variants={circleTopRightVariants}
         initial="hidden"
+        exit="exit"
+        repeat="infinity"
       />
       <motion.circle
         cx="2"
@@ -76,8 +75,10 @@ const CircleFusion = () => {
         r="2"
         fill="#ffffff"
         animate={controls}
-        variants={cornerCirclesVariants}
+        variants={circleBottomLeftVariants}
         initial="hidden"
+        exit="exit"
+        repeat="infinity"
       />
       <motion.circle
         cx="8"
@@ -85,12 +86,32 @@ const CircleFusion = () => {
         r="2"
         fill="#ffffff"
         animate={controls}
-        variants={cornerCirclesVariants}
+        variants={circleBottomRightVariants}
         initial="hidden"
+        exit="exit"
+        repeat="infinity"
       />
+    </svg>
+  );
+};
 
-      {/* Círculo pequeño en el centro */}
-      <motion.circle
+export default CircleFusion;
+
+/*   const centerCircleVariants = {
+    hidden: { scale: 0, opacity: 0 },
+    visible: { scale: 1, opacity: 1 },
+  };
+
+  const enlargedCircleVariants = {
+    hidden: { scale: 1, opacity: 1 },
+    visible: { scale: 2, fill: "#ffffff" },
+  }; */
+
+{
+  /* Círculo pequeño en el centro */
+}
+{
+  /* <motion.circle
         cx="5"
         cy="5"
         r="1"
@@ -100,10 +121,14 @@ const CircleFusion = () => {
         animate={controls}
         onAnimationComplete={animateCentralCircle}
         transition={{ duration: 1 }}
-      />
+      /> */
+}
 
-      {/* Círculo grande que crece y cambia de color */}
-      <motion.circle
+{
+  /* Círculo grande que crece y cambia de color */
+}
+{
+  /* <motion.circle
         cx="5"
         cy="5"
         r="2"
@@ -113,9 +138,17 @@ const CircleFusion = () => {
         animate={controls}
         exit={{ opacity: 0, scale: 1, fill: "#808080" }}
         transition={{ duration: 2 }}
-      />
-    </svg>
-  );
+      /> */
+}
+const animateCentralCircle = async () => {
+  // Círculo central se agranda
+  // await controls.start("visible");
+  // Espera un momento antes de iniciar la siguiente animación
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  // Círculo grande cambia de color a gris, se achica y desaparece
+  // await controls.start({ fill: "#808080", scale: 1, opacity: 0 });
+  // Espera un momento antes de iniciar la siguiente animación
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  // Reinicia la animación del círculo central volviendo al estado inicial
+  // controls.start(centerCircleVariants.hidden);
 };
-
-export default CircleFusion;
