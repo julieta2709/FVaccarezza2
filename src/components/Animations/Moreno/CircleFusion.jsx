9db8cb649ba5997dde4e499e43fa3e2d58fa40ff
Julieta Gordillo <julietagordillo13@hotmail.com>
2024-01-24 14:21:58 -0300

    circle animation

--- src/components/Animations/Moreno/CircleFusion.jsx
+++ src/components/Animations/Moreno/CircleFusion.jsx
@@ -13,17 +13,41 @@ const CircleFusion = () => {
     visible: { scale: 2, fill: "#ffffff" },
   };
 
+  const cornerCirclesVariants = {
+    hidden: { x: 2, y: 2, scale: 2, opacity: 1 },
+    visible: { x: 5, y: 5, scale: 0, opacity: 0 },
+  };
+
   const controls = useAnimation();
 
+  const animateCentralCircle = async () => {
+    // Círculo central se agranda
+    await controls.start("visible");
+
+    // Espera un momento antes de iniciar la siguiente animación
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Círculo grande cambia de color a gris, se achica y desaparece
+    await controls.start({ fill: "#808080", scale: 1, opacity: 0 });
+
+    // Espera un momento antes de iniciar la siguiente animación
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Reinicia la animación del círculo central volviendo al estado inicial
+    controls.start(centerCircleVariants.hidden);
+  };
+
   useEffect(() => {
-    const animateCircles = async () => {
-      await controls.start({ x: 5, y: 5 });
-      await controls.start("visible");
-      controls.start("hidden");
+    const animateCornerCircles = async () => {
+      // Mueve los círculos de las esquinas hacia el centro
+      await controls.start(cornerCirclesVariants.visible);
+
+      // Reinicia la animación de los círculos de las esquinas volviendo al estado inicial
+      controls.start(cornerCirclesVariants.hidden);
     };
 
-    animateCircles();
-  }, [controls]);
+    animateCornerCircles();
+  }, [controls, cornerCirclesVariants]);
 
   return (
     <svg width="10" height="10" className="circle-AnimaContainer">
@@ -33,36 +57,36 @@ const CircleFusion = () => {
         cy="2"
         r="2"
         fill="#ffffff"
-        whileInView={controls}
-        initial={{ x: 2, y: 2 }}
-        viewport={{ once: true }}
+        animate={controls}
+        variants={cornerCirclesVariants}
+        initial="hidden"
       />
       <motion.circle
         cx="8"
         cy="2"
         r="2"
         fill="#ffffff"
-        whileInView={controls}
-        initial={{ x: 2, y: 2 }}
-        viewport={{ once: true }}
+        animate={controls}
+        variants={cornerCirclesVariants}
+        initial="hidden"
       />
       <motion.circle
         cx="2"
         cy="8"
         r="2"
         fill="#ffffff"
-        whileInView={controls}
-        initial={{ x: 2, y: 2 }}
-        viewport={{ once: true }}
+        animate={controls}
+        variants={cornerCirclesVariants}
+        initial="hidden"
       />
       <motion.circle
         cx="8"
         cy="8"
         r="2"
         fill="#ffffff"
-        whileInView={controls}
-        initial={{ x: 2, y: 2 }}
-        viewport={{ once: true }}
+        animate={controls}
+        variants={cornerCirclesVariants}
+        initial="hidden"
       />
 
       {/* Círculo pequeño en el centro */}
@@ -73,10 +97,11 @@ const CircleFusion = () => {
         fill="#ffffff"
         variants={centerCircleVariants}
         initial="hidden"
-        whileInView="visible"
+        animate={controls}
+        onAnimationComplete={animateCentralCircle}
         transition={{ duration: 1 }}
-        viewport={{ once: true }}
       />
+
       {/* Círculo grande que crece y cambia de color */}
       <motion.circle
         cx="5"
@@ -85,11 +110,10 @@ const CircleFusion = () => {
         fill="#ffffff"
         variants={enlargedCircleVariants}
         initial="hidden"
-        whileInView="visible"
+        animate={controls}
+        exit={{ opacity: 0, scale: 1, fill: "#808080" }}
         transition={{ duration: 2 }}
-        exit={{ opacity: 1, scale: 1, fill: "#808080" }}
-        viewport={{ once: true }}
-       />
+      />
     </svg>
   );
 };
