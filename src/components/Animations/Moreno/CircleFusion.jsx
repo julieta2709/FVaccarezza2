982fbb78002d9e367c01627f0329d240cae54f4d
Julieta Gordillo <julietagordillo13@hotmail.com>
2024-01-25 09:39:20 -0300

    circle animation

--- src/components/Animations/Moreno/CircleFusion.jsx
+++ src/components/Animations/Moreno/CircleFusion.jsx
@@ -1,92 +1,97 @@
 import { motion } from "framer-motion";
-import * as React from "react";
+import React, { useState } from "react";
 import "../Moreno/circle.css";
-
-/* const container = {
-  visible: {
-    transition: {
-      staggerChildren: 0.5,
-      staggerDirection: -1,
-    },
-  },
-};
-
-const item = {
-  hidden: {
-    y: 0,
-    x: 0,
-    opacity: 1,
-  },
-  visible: (i) => {
-    const distance = 10;
-    const diagonalDistance = Math.sqrt(2) * distance;
-
-    return {
-      y: i === 0 || i === 3 ? -diagonalDistance : diagonalDistance,
-      x: i === 0 || i === 1 ? -diagonalDistance : diagonalDistance,
-      opacity: 1,
-      transition: {
-        type: 'spring',
-        damping: 15,
-        stiffness: 300,
-      },
-    };
-  },
-};
-const CircleFusion = () => (
-  <motion.ul
-    className="Circlecontainer"
-    variants={container}
-    initial="hidden"
-    animate="visible"
-  >
-    {[0, 1, 2, 3].map((index) => (
-      <motion.li key={index} className="item" variants={item} custom={index} />
-    ))}
-  </motion.ul>
-);
-export default CircleFusion;
- */
-
+/* 
 const CircleFusion = () => {
   return (
-    <div
-      style={{
-        width: "200px",
-        height: "200px",
-        backgroundColor: "lightblue",
-        position: "relative",
-      }}
-    >
+    <div className="Circlecontainer">
       <Circle x={0} y={0} />
-      <Circle x={200} y={0} />
-      <Circle x={0} y={200} />
-      <Circle x={200} y={200} />
+      <Circle x={50} y={0} />
+      <Circle x={0} y={50} />
+      <Circle x={50} y={50} />
     </div>
   );
 };
 
 const Circle = ({ x, y }) => {
-  const diagonalX = 100 - x; // Calcular la coordenada x del centro menos la coordenada x del círculo
-  const diagonalY = 100 - y;
+  const originalPosition = { x: x, y: y };
+  const diagonalX = 25 - x;
+  const diagonalY = 25 - y;
+
   return (
     <motion.div
+      className="item"
       style={{
-        width: "20px",
-        height: "20px",
-        backgroundColor: "red",
-        borderRadius: "50%",
-        position: "absolute",
         top: y,
         left: x,
       }}
       animate={{
+        x: [x, originalPosition.x],
+        y: [y, originalPosition.y],
+      }}
+      transition={{
+        duration: 2,
+      }}
+      exit={{ top: y, left: x }}
+      whileInView={{
+        x: diagonalX,
+        y: diagonalY,
+      }}
+    ></motion.div>
+  );
+};
+
+export default CircleFusion;
+ */
+const CircleFusion = () => {
+  const originalPositions = [
+    { x: 0, y: 0 },
+    { x: 50, y: 0 },
+    { x: 0, y: 50 },
+    { x: 50, y: 50 },
+  ];
+
+  const [circlePositions, setCirclePositions] = useState(originalPositions);
+
+  const resetCirclePositions = () => {
+    setCirclePositions(originalPositions);
+  };
+
+  return (
+    <div className="Circlecontainer">
+      {circlePositions.map((position, index) => (
+        <Circle
+          key={index}
+          x={position.x}
+          y={position.y}
+          resetCirclePositions={resetCirclePositions}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Circle = ({ x, y, resetCirclePositions }) => {
+  const diagonalX = 25 - x;
+  const diagonalY = 25 - y;
+  return (
+    <motion.div
+      className="item"
+      style={{
+        top: y,
+        left: x,
+      }}
+      whileInView={{
         x: diagonalX,
         y: diagonalY,
       }}
       transition={{
         duration: 2,
       }}
+      exit={{
+        onComplete: resetCirclePositions,
+      }}
+      viewport={{ once: true }}
     ></motion.div>
   );
 };
