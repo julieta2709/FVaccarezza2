7476965827181ce2656f2072e869ed9667073a1c
Julieta Gordillo <julietagordillo13@hotmail.com>
2024-02-29 23:27:29 -0300

    fix styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,22 @@ import RedCircle from "../assets/img/icons/RedCircle.svg";
 import "../styles/Home.css";
 
 const Home = () => {
+
+  const scrollToAbout = () => {
+    const aboutSection = document.getElementById("about");
+    aboutSection.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <div className="Home-container">
       <img src={HomeLeft} alt="HomeLeftBg" className="HomeBg" />
       <div className="Home-WidthContainer">
-      <div className="HomePF-Container">
-        <img src={RedCircle} alt="RedCircle" className="redCircle" />
-        <h1 className="portfolio">PORTFOLIO.</h1>
-        <h2 className="year">2024</h2>
-        <p className="fv">FEDERICO VACCAREZZA</p>
-        <img src={RedCircle} alt="RedCircle" className="redCircle" />
-        </div>
+          <div className="HomePF-Container" onClick={scrollToAbout}>
+            <img src={RedCircle} alt="RedCircle" className="redCircle" />
+            <h1 className="portfolio">PORTFOLIO.</h1>
+            <h2 className="year">2024</h2>
+            <p className="fv">FEDERICO VACCAREZZA</p>
+            <img src={RedCircle} alt="RedCircle" className="redCircle" />
+          </div>
       </div>
     </div>
   );
